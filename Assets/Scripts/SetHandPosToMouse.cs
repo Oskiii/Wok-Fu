5436e0c4f699386c62527b6c49761caa7d76d0e0
Oskiii <oliukku@gmail.com>
2016-12-15 23:22:31 +0200

    animating

---
+++ Assets/Scripts/SetHandPosToMouse.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetHandPosToMouse : MonoBehaviour {
+
+	private Vector3 mousePosition;
+	public float moveSpeed = 0.1f;
+	public GameObject shoulder;
+	private float maxDistance;
+	private Rigidbody2D rb;
+	
+	void Start (){
+
+		maxDistance = (gameObject.transform.position - shoulder.transform.position).magnitude;
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	void Update () {
+		mousePosition = Input.mousePosition;
+		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
+
+		print (maxDistance);
+	
+		rb.velocity = GetDirection ();
+		//rb.AddForce (GetDirection () * 100);
+		//if ((mousePosition - (shoulder.transform.position)).magnitude < maxDistance) {
+			
+			//transform.position = Vector2.Lerp (transform.position, mousePosition, moveSpeed);
+		//}
+	}
+
+	Vector2 GetDirection(){
+		return new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical")).normalized;
+	}
+}
